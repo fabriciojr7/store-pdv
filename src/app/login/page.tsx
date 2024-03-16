2fa6924a725f2e5630a6a16664751a26594e2dd1
Fabrício Júnior <fabriciojunior77@outlook.com>
2024-03-16 14:01:34 -0300

    feat:initial login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Logo } from "@/components/Logo";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-96 p-10 rounded-2xl bg-zinc-950 space-y-8">
+        <Logo />
+
+        <div>
+          <Button>Entrar</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
