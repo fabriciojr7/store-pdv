d2af3d9d5c28c1cc143d899f350bc5696573a782
Fabrício Júnior <fabriciojunior77@outlook.com>
2024-03-21 17:41:26 -0300

    fix:refactoring authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
+import { authOptions } from "@/lib/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
